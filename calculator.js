e87d940c2edb13a5d6a0671ed6c00d6797daa605
ronak2030 <webronak2030@gmail.com>
2024-11-28 20:41:25 +0530

    test: write test to handle comma separated numbers in string

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 const calculator = (str) => {
     if(str === "") return 0;
     if(str.length === 1) return parseInt(str);
+    if(str.includes(",")){
+        return str.split(",").reduce((acc, numStr) => acc += parseInt(numStr), 0)
+    }
 }
 
 module.exports = calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,7 @@
 const calculate = require("./calculator");
 
 describe("String Calculator", () => {
-  it("Should return 0 if input is empty string", () => {
+  it('Should return 0 if input is empty string', () => {
     const result = calculate("");
     expect(result).toBe(0);
   });
@@ -10,4 +10,9 @@ describe("String Calculator", () => {
     const result = calculate("5");
     expect(result).toBe(5);
   });
+
+  it('Should return addition of comma saperated two numbers', () => {
+    const result = calculate("1,2");
+    expect(result).toBe(3);
+  })
 });
